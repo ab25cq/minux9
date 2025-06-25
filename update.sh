ae6dd5590cdddd35902b31970a5dafdea8b34fc1
ab25cq <ab25cq@icloud.com>
2025-06-26 04:54:06 +0900

    iii

--- update.sh
+++ update.sh
@@ -6,16 +6,10 @@ git config --global user.name ab25cq
 
 #sh remove_all_build.sh
 
-#git add .
-#git commit 
-#git remote set-url origin git@github.com:ab25cq/minux9.git
-#git push --force origin master
-
-
-echo "# minux9" >> README.md
-git init
 git add .
 git commit 
-git branch -M main
-git remote set-url origin https://github.com/ab25cq/minux9.git
-git push -u origin main
+git remote set-url origin git@github.com:ab25cq/minux9.git
+git push --force origin master
+
+
+
