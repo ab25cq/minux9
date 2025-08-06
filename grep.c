4f28ec487f53abd713a666a6f6a81a5eaa23d18b
ab25cq <ab25cq@icloud.com>
2025-08-06 19:51:58 +0900

    ii

--- grep.c
+++ grep.c
@@ -1,4 +1,6 @@
 #include "minux.h"
+#include <stdint.h>
+#include <stdarg.h>
 
 int strlen(const char *s) {
   int n;
@@ -19,6 +21,206 @@ int memcmp(const void *v1, const void *v2, unsigned int n) {
         s1++, s2++;
     }
 
+    return 0;
+}
+
+void putchar(char c)
+{
+    char d = c;
+    write(1, &d, 1);
+}
+
+void puts(const char *s) {
+    while (*s) {
+        putchar(*s++);
+    }
+}
+
+void printint(int val_, int base, int sign) {
+    char buf[33];  
+    int i = 0;
+    int negative = 0;
+    unsigned int uval;
+
+    if (sign && val_ < 0) {
+        negative = 1;
+        uval = -val_;
+    } else {
+        uval = (unsigned int)val_;
+    }
+
+    if (uval == 0) {
+        putchar('0');
+        return;
+    }
+
+    while (uval > 0) {
+        int digit = uval % base;
+        buf[i++] = digit < 10 ? '0' + digit : 'a' + (digit - 10);
+        uval /= base;
+    }
+
+    if (negative) {
+        putchar('-');
+    }
+
+    while (--i >= 0) {
+        putchar(buf[i]);
+    }
+}
+
+void printlong(unsigned long val_, int base, int sign)  {
+    char buf[65];  
+    int i = 0;
+    int negative = 0;
+
+    if (sign && (long)val_ < 0) {
+        negative = 1;
+        val_ = -(long)val_;
+    }
+
+    if (val_ == 0) {
+        putchar('0');
+        return;
+    }
+
+    while (val_ > 0) {
+        int digit = val_ % base;
+        buf[i++] = digit < 10 ? '0' + digit : 'a' + (digit - 10);
+        val_ /= base;
+    }
+
+    if (negative) {
+        putchar('-');
+    }
+
+    while (--i >= 0) {
+        putchar(buf[i]);
+    }
+}
+
+void printlonglong(unsigned long long val_, int base, int sign)  {
+    char buf[65];
+    int i = 0;
+    int negative = 0;
+
+    if (sign && (long long)val_ < 0) {
+        negative = 1;
+        val_ = -(long long)val_;
+    }
+
+    if (val_ == 0) {
+        putchar('0');
+        return;
+    }
+
+    while (val_ > 0) {
+        int digit = val_ % base;
+        buf[i++] = digit < 10 ? '0' + digit : 'a' + (digit - 10);
+        val_ /= base;
+    }
+
+    if (negative) {
+        putchar('-');
+    }
+
+    while (--i >= 0) {
+        putchar(buf[i]);
+    }
+}
+
+int printf(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    const char* p;
+    for (p = fmt; *p; p++) {
+        if (*p != '%') {
+            putchar(*p);
+            continue;
+        }
+
+        p++; 
+
+        if (*p == 'l') {
+            int lcount = 1;
+            if (*(p+1) == 'l') {
+                lcount = 2;
+                p++;
+            }
+            p++;
+
+            switch (*p) {
+                case 'x': {
+                    if (lcount == 1) {
+                        unsigned long val_ = va_arg(ap, unsigned long);
+                        printlong(val_, 16, 0);
+                    } else {
+                        unsigned long long val_ = va_arg(ap, unsigned long long);
+                        printlonglong(val_, 16, 0);
+                    }
+                    break;
+                }
+                case 'd': {
+                    if (lcount == 1) {
+                        long val_ = va_arg(ap, long);
+                        printlong(val_, 10, 1);
+                    } else {
+                        long long val_ = va_arg(ap, long long);
+                        printlonglong(val_, 10, 1);
+                    }
+                    break;
+                }
+                default: {
+                    putchar('%');
+                    for (int i=0; i<lcount; i++) putchar('l');
+                    putchar(*p);
+                    break;
+                }
+            }
+        } else {
+            switch (*p) {
+                case 'd': {
+                    int val_ = va_arg(ap, int);
+                    printint(val_, 10, 1);
+                    break;
+                }
+                case 'x': {
+                    unsigned int val_ = va_arg(ap, unsigned int);
+                    printint(val_, 16, 0);
+                    break;
+                }
+                case 'p': {
+                    unsigned long val_ = (unsigned long)va_arg(ap, void*);
+                    putchar('0'); putchar('x');
+                    printlong(val_, 16, 0);
+                    break;
+                }
+                case 's': {
+                    const char* s = va_arg(ap, const char*);
+                    if (!s) s = "(null)";
+                    while (*s) putchar(*s++);
+                    break;
+                }
+                case 'c': {
+                    char c = (char)va_arg(ap, int);
+                    putchar(c);
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    break;
+                }
+                default: {
+                    putchar('%');
+                    putchar(*p);
+                    break;
+                }
+            }
+        }
+    }
+
+    va_end(ap);
     return 0;
 }
             
@@ -45,8 +247,8 @@ int main(int argc, char** argv) {
         else if(memcmp(p, candidate, strlen(candidate)) == 0) {
             char* tail;
             while(*p != '\0' && *p != '\n') {
-                tail = p;
                 p++;
+                tail = p;
             }
             write(1, head, tail -head);
             write(1, "\r\n", 2);
